8b956e7ca839a1bb1c4994e1ad9e20397f41e25a
SamuelMettler <samuel@mettlerpm.com>
2019-04-14 19:11:50 +0200

    representation classe + modif code

--- Parsing_XML/main.py
+++ Parsing_XML/main.py
@@ -9,6 +9,10 @@ prot_seq = ["LOCATE_protein/protein/protein_sequence",
 loc_seq = ["LOCATE_protein/scl_prediction/source/location",
            "LOCATE_protein/scl_prediction/source/goid"]
 
+prot_loc_seq = ["LOCATE_protein/protein/protein_sequence",
+                "LOCATE_protein/transcript/transcript_sequence",
+                "LOCATE_protein/scl_prediction/source/goid"]
+
 loc_seq_tier = ["LOCATE_protein/literature/reference/locations/location"]
 
 nbProt = os.popen("grep -o '<LOCATE_protein*' minimouse.xml | wc -l").read()
@@ -29,7 +33,8 @@ organism = "LOCATE_protein/protein/organism"
 tree = ET.parse("minimouse.xml")
 root = tree.getroot();
 
-"""
+# peuplage table proteine
+print "Peuplage table proteine"
 for i in range(0, len(prot_seq)):
     j = 0
     for element in root.findall(prot_seq[i]):
@@ -37,20 +42,13 @@ for i in range(0, len(prot_seq)):
         j += 1
 id = 0
 for row in res:
-    output = "INSERT INTO protein(id,sprt,sadn) VALUES(" + str(id) + ",'" + row[0] + "','" + row[1] + "');"
+    output = "INSERT INTO protein(id,sprt,sadn) VALUES(" + str(id) + ",'" + row[0] + "','" + row[1] + "');" + '\n'
     id += 1
+    print output
     file.write(output)
-"""
 
-#trouver les cas classiques
-for i in range(0, len(loc_seq)):
-    j = 0
-    for element in root.findall(loc_seq[i]):
-        if element.text != "No prediction":
-            res2[j][i] = element.text
-            j += 1
-
-""" pas utilisé car blc
+#--------
+""" pas utilise car c'est les precidictions
 #trouver tous les tiers 1/2
 for i in range(0, len(loc_seq_tier)):
     for element in root.findall(loc_seq_tier[0]):
@@ -58,6 +56,17 @@ for i in range(0, len(loc_seq_tier)):
         for child in element:
             print child.tag, child.text
 """
+
+#---------
+# peuplage table Localisation
+print "Peuplage table loc"
+# trouver toute les paire go id nom
+for i in range(0, len(loc_seq)):
+    j = 0
+    for element in root.findall(loc_seq[i]):
+        if element.text != "No prediction":
+            res2[j][i] = element.text
+            j += 1
 
 id = 0
 goName = {}
@@ -67,13 +76,19 @@ for row in res2:
     if goID not in goName :
         if goID != "" :
             goName[goID] = row[0]
-    output = "INSERT INTO localisation(id,name,goid) VALUES(" + str(id) + ",'" + row[0] + "','" + row[1] + "');"
-    #id += 1
-    #print(output)
-    #file2.write(output)
 
 
-#print(goName)
+for key in goName:
+    output = "INSERT INTO localisation(id,name,goid) VALUES(" + str(id) + ",'" + "GO:" + str(key) + "','"+ goName[key] + "');" + '\n'
+    id += 1
+    print output
+    file2.write(output)
+#-------------
+# peuplage table PROTLOC
+
+#incrementer de 1 par 1 notre counter par LOC
+
+
 file.close()
 file2.close()
 
